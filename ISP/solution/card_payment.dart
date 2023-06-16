a80ad17a91c422ef1db4973ab738be17891f2b48
bishal5917 <poudelb172@gmail.com>
2023-06-16 16:58:08 +0545

    ISP solution

---
+++ ISP/solution/card_payment.dart
@@ -0,0 +1,31 @@
+import 'payment_service.dart';
+
+class CardPayment implements PaymentService {
+  @override
+  void cashBack() {
+    // TODO: implement cashBack
+  }
+
+  @override
+  void installment() {
+    // TODO: implement installment
+  }
+
+  @override
+  void pay() {
+    // TODO: implement pay
+  }
+
+  @override
+  void payCash() {
+    // TODO: implement payCash
+  }
+
+  @override
+  void payCoupon() {
+    // TODO: implement payCoupon
+  }
+}
+
+// Solution
+// here the card payment doesnot have to define the unwanted methods
---
+++ ISP/solution/payment_service.dart
@@ -0,0 +1,11 @@
+abstract class PaymentService {
+  void pay();
+
+  void installment();
+
+  void payCash();
+
+  void cashBack();
+
+  void payCoupon();
+}
